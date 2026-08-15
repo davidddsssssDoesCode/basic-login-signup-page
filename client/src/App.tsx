3ba637087e52d41c1a2fdabf050185dcd912fbb2
David Visaya <davidvisaya568@gmail.com>
2026-08-15 20:44:49 +0800

    Added auth page design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
+import { Navigate, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import LogInPage from "./pages/AuthenticationPage";
+
 export default function App() {
   return (
     <main>
-      
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth" replace />} />
+        <Route path="/auth" element={<LogInPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </main>
   );
 }
---
+++ client/src/pages/AuthenticationPage.tsx
@@ -0,0 +1,87 @@
+import {useState} from "react";
+
+export default function LogInPage() {
+  const [formState, setFormState] = useState<string>("login");
+
+  function logIn(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  function signUp(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <main className="h-screen flex justify-center items-center bg-blue-200 p-5">
+      <form
+        onSubmit={formState === "login" ? logIn : signUp}
+        className={`h-120 w-90 flex flex-col justify-center items-center gap-5 rounded-4xl bg-white p-5 border sm:h-125 sm:w-105 transition-transform duration-200 ease-in-out ${formState === "login" ? "rotate-y-0" : "rotate-y-180"}`}
+      >
+        {formState === "login" ? (
+          <>
+            <h1 className="text-5xl">Log-In</h1>
+            <div className="flex flex-col items-center gap-4">
+              <input
+                type="text"
+                placeholder="Username"
+                className="h-13 w-65 border-b p-3 outline-0 transition-all duration-200 ease-in-out focus:scale-[1.02] focus:border-b-blue-800 sm:w-75"
+              />
+              <input
+                type="text"
+                placeholder="Password"
+                className="h-13 w-65 border-b p-3 outline-0 transition-all duration-200 ease-in-out focus:scale-[1.02] focus:border-b-blue-800 sm:w-75"
+              />
+              <button className="h-13 w-20 rounded-full bg-sky-300 border transition-all duration-200 ease-in-out hover:bg-sky-400 hover:scale-[1.05] active:bg-sky-500 active:scale-[0.95]">
+                Log-In
+              </button>
+            </div>
+            <div className="flex flex-col items-center">
+              <p>Don't have an account?</p>
+              <a
+                onClick={() => setFormState("signup")}
+                className="text-purple-700 transition-colors duration-200 ease-in-out hover:text-purple-900 cursor-pointer active:text-purple-950"
+              >
+                Sign-Up
+              </a>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="rotate-y-180 flex flex-col items-center gap-5">
+              <h1 className="text-5xl">Sign-Up</h1>
+              <div className="flex flex-col items-center gap-4">
+                <input
+                  type="text"
+                  placeholder="New Username"
+                  className="h-13 w-65 border-b p-3 outline-0 transition-all duration-200 ease-in-out focus:scale-[1.02] focus:border-b-blue-800 sm:w-75"
+                />
+                <input
+                  type="text"
+                  placeholder="New Password"
+                  className="h-13 w-65 border-b p-3 outline-0 transition-all duration-200 ease-in-out focus:scale-[1.02] focus:border-b-blue-800 sm:w-75"
+                />
+                <input
+                  type="text"
+                  placeholder="Confirm Password"
+                  className="h-13 w-65 border-b p-3 outline-0 transition-all duration-200 ease-in-out focus:scale-[1.02] focus:border-b-blue-800 sm:w-75"
+                />
+                <button className="h-13 w-20 rounded-full bg-sky-300 border transition-all duration-200 ease-in-out hover:bg-sky-400 hover:scale-[1.05] active:bg-sky-500 active:scale-[0.95]">
+                  Sign-Up
+                </button>
+              </div>
+              <div className="flex flex-col items-center">
+                <p>Don't have an account?</p>
+                <a
+                  onClick={() => setFormState("login")}
+                  className="text-purple-700 transition-colors duration-200 ease-in-out hover:text-purple-900 cursor-pointer active:text-purple-950"
+                >
+                  Log-In
+                </a>
+              </div>
+            </div>
+          </>
+        )}
+      </form>
+    </main>
+  );
+}
