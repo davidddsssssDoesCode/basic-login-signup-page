8652205410553144f16721cdbf73daf1b21fd2a3
David Visaya <davidvisaya568@gmail.com>
2026-08-16 16:50:26 +0800

    Added login

--- server/dist/routes/users.js
+++ server/dist/routes/users.js
@@ -1,7 +1,27 @@
 import express, {} from "express";
+import pool from "../config/pool.js";
 const router = express.Router();
-router.get("/get", (req, res) => {
-    res.status(200).json({ message: "Ping success!" });
+router.get("/login", async (req, res) => {
+    try {
+        const sumbittedData = req.body;
+        const results = await pool.query("SELECT * FROM users WHERE username = $1 AND password = $2", [sumbittedData.username, sumbittedData.password]);
+        if (results.rowCount === 0) {
+            return res.status(404).json({
+                message: "Username and/or password is invalid.",
+                status: false,
+            });
+        }
+        res.status(200).json({
+            message: "Logging you in...",
+            status: true,
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            message: "There was a problem logging you in.",
+            status: false,
+        });
+    }
 });
 export default router;
 //# sourceMappingURL=users.js.map
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,9 +1,40 @@
 import express, {type Request, type Response} from "express";
+import pool from "../config/pool.js";
 
 const router = express.Router();
 
-router.get("/get", (req: Request, res: Response) => {
-    res.status(200).json({message : "Ping success!"});
+type User = {
+  id?: number;
+  username: string;
+  password: string;
+};
+
+router.get("/login", async (req: Request, res: Response) => {
+  try {
+    const sumbittedData: User = req.body;
+
+    const results = await pool.query<User>(
+      "SELECT * FROM users WHERE username = $1 AND password = $2",
+      [sumbittedData.username, sumbittedData.password],
+    );
+
+    if (results.rowCount === 0) {
+      return res.status(404).json({
+        message: "Username and/or password is invalid.",
+        status: false,
+      });
+    }
+
+    res.status(200).json({
+      message: "Logging you in...",
+      status: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+        message: "There was a problem logging you in.",
+        status: false,
+    });
+  }
 });
 
 export default router;
